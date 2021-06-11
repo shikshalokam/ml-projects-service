3318f6db60d15e2e6e378d9584889dd51a69e6c7
aman-tunerlabs <aman@tunerlabs.com>
2021-06-11 19:31:10 +0530

    Added read preference in replica set

--- config/db/mongodb.js
+++ config/db/mongodb.js
@@ -26,8 +26,12 @@ const DB = function() {
 
   let options = {useNewUrlParser: true};
 
-  if ( process.env.REPLICA_SET_NAME && process.env.REPLICA_SET_NAME !== "") {
+  if (process.env.REPLICA_SET_NAME && process.env.REPLICA_SET_NAME !== "") {
     options["replset"] = {rs_name: process.env.REPLICA_SET_NAME };
+
+    if (process.env.REPLICA_SET_READ_PREFERENCE && process.env.REPLICA_SET_READ_PREFERENCE !== "") {
+      options["replset"]["readPreference"] = process.env.readPreference;
+    }
   }
   
   const db = mongoose.createConnection(
