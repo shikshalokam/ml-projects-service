ef29cd13a027efecea1174994565fec1eb3d6f94
aman-tunerlabs <aman@tunerlabs.com>
2021-07-30 11:36:37 +0530

    Mongodb health check issue

--- healthCheck/mongodb.js
+++ healthCheck/mongodb.js
@@ -20,6 +20,7 @@ function health_check() {
             return resolve(false)
         });
         db.once("open", function() {
+            db.close(function(){});
             return resolve(true);    
         });
     })
