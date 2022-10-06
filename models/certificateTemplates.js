e6fea80bccdad4c98fbd6f857483fc5c6c73389f
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2022-10-06 11:54:14 +0530

    certificate template schema updated

--- models/certificateTemplates.js
+++ models/certificateTemplates.js
@@ -1,14 +1,14 @@
 module.exports = {
-    name: "certificateTemplates",
-    schema: {
-      templateUrl: {
-        type : String,
-        index : true
-      },
-      issuer: Object,
-      status: String,
-      solutionId: "ObjectId",
-      programId: "ObjectId",
-      criteria: Object
-    }
+  name: "certificateTemplates",
+  schema: {
+    templateUrl: String,
+    issuer: Object,
+    status: String,
+    solutionId: {
+      type : "ObjectId",
+      index : true
+    },
+    programId: "ObjectId",
+    criteria: Object
+  }
 };
