8b3de75febf4aa5c1326574e479f019dc7dfd6a4
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2022-12-13 21:38:23 +0530

    kafka push for sync mode removed

--- generics/constants/api-responses.js
+++ generics/constants/api-responses.js
@@ -133,5 +133,6 @@ module.exports = {
     "CERTIFICATE_TEMPLATE_NOT_FOUND" : "Certificate template details not found",
     "CERTIFICATE_GENERATION_FAILED" : "Certificate generation failed",
     "NOT_ELIGIBLE_FOR_CERTIFICATE" : "Project is not eligible for certificate",
-    "ISSUER_KID_NOT_FOUND" : "Failed to fetch certificate issuer kid"
+    "ISSUER_KID_NOT_FOUND" : "Failed to fetch certificate issuer kid",
+    "PROJECT_SUBMITTED_FOR_REISSUE" : "Submitted for project certificate reIssue"
 };
