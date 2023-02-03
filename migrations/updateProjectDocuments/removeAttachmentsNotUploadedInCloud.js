da2a6025157a5becaf254435315a48e8e85487d8
ankitshahu <ankit@matchlog.delivery>
2023-02-03 21:41:20 +0530

    Updated Code

--- migrations/updateProjectDocuments/removeAttachmentsNotUploadedInCloud.js
+++ migrations/updateProjectDocuments/removeAttachmentsNotUploadedInCloud.js
@@ -4,7 +4,7 @@
  * created-date : 02-Feb-2023
  * Description : Migration script for update project
  */
-
+ "use strict";
 const path = require("path");
 let rootPath = path.join(__dirname, '../../')
 require('dotenv').config({ path: rootPath+'/.env' })
@@ -58,12 +58,15 @@ const filePathUrl = "https://samikshaprod.blob.core.windows.net/samiksha/";
                 let id = projectId[projectIdpointer];
                 
                 //pull project Data from DB 
-                let pullProjectData = await db.collection('projects').find({
+                let pullProjectDataDB = await db.collection('projects').find({
                     _id: id
                 }).project({}).toArray({})
 
                 //store in varibale to avoid using [0]
-                pullProjectData = pullProjectData[0]
+                const pullProjectData = pullProjectDataDB[0]
+
+                const allTasksBeforeValidation = JSON.stringify(pullProjectData.tasks)
+                let allAttachmentsBeforeValidation = ""
 
                 //update Object
                 let updateObject = {
@@ -72,56 +75,74 @@ const filePathUrl = "https://samikshaprod.blob.core.windows.net/samiksha/";
 
                 //check if project Document has attachments or not if present then checks length of attachment it should be greater than 0
                 if( pullProjectData.hasOwnProperty('attachments') && pullProjectData.attachments.length > 0 ) { 
-
+                    allAttachmentsBeforeValidation = JSON.stringify(pullProjectData.attachments)
                     //assign attachmentPresent object to update variable
-                    updateObject['$set']['attachments'] = await validatedAttachments( pullProjectData.attachments)
+                    updateObject['$set']['attachments'] = await validatedAttachments(pullProjectData.attachments)
                 }
 
                 //check if project has tasks available and length of tasks array should be greater than 0   
                 if( pullProjectData.hasOwnProperty('tasks') && pullProjectData.tasks.length > 0){
 
-
-                    //varibale to store updated task objects
-                    let newTaskWithValidatedAttachments = []
+                    let tasks = pullProjectData.tasks
+                    updateObject['$set']['tasks'] = [...tasks]
+                    
                     //for loop for each task
-                    for(let taskCounter = 0; taskCounter< pullProjectData.tasks.length; taskCounter++){
-
-                        //store task object in task
-                        let task = pullProjectData.tasks[taskCounter]
+                    for(let taskCounter = 0; taskCounter< tasks.length; taskCounter++){
 
                         //checks if task object has key attachment present or not
-                        if(task.hasOwnProperty("attachments")){
-
+                        if(tasks[taskCounter].hasOwnProperty("attachments") && tasks[taskCounter].attachments.length>0){
                             //assign attachmentPresent array to task attachments
-                            task.attachments = await validatedAttachments(task.attachments)
+                            updateObject['$set']['tasks'][taskCounter].attachments = await validatedAttachments((updateObject['$set']['tasks'][taskCounter].attachments))
                         }
-                        //push task to new array of tasks key
-                        newTaskWithValidatedAttachments.push(task)
                     }
-                    //assign all valid task to task for update
-                    updateObject['$set']['tasks'] = newTaskWithValidatedAttachments
                 }
+            
                 //push project id which is validated
-    
-                UpdatedProjectId.push(id)
-                //update project with new varibales
-                await db.collection('projects').updateOne({_id:id},updateObject)
+                if(JSON.stringify(updateObject["$set"]["tasks"]) != allTasksBeforeValidation || JSON.stringify(updateObject["$set"]["attachments"]) != allAttachmentsBeforeValidation){
+                  
+                    if(JSON.stringify(updateObject["$set"]["tasks"]) == allTasksBeforeValidation){
+                        delete updateObject["$set"].tasks
+                    }
+                    if(JSON.stringify(updateObject["$set"]["attachments"]) == allAttachmentsBeforeValidation){
+                        delete updateObject["$set"].attachments
+                    }
+                    if(updateObject["$set"].hasOwnProperty("attachments") || updateObject["$set"].hasOwnProperty("tasks")){
+                    UpdatedProjectId.push(id)
+                    //update project with new varibales
+                    await db.collection('projects').updateOne({_id:id},updateObject)}
+                }
             }
             
             
         }
         console.log(UpdatedProjectId)
 
+       
+
+        fs.writeFile(
+            'updatedProjectWithAttachments.json',
+
+            JSON.stringify({updatedProjectIds: UpdatedProjectId}),
+
+            function (err) {
+                if (err) {
+                    console.error('Crap happens');
+                }
+            }
+        );
+
+
+
         async function validatedAttachments(attachments){
             //varibale to store updated attachment objects
             let attachmentsPresent = []
-
+        
             //for loop for each attachment
-            for(let attachmentCounter = 0; attachmentCounter< attachments.length; attachmentCounter++){
-
+            for(let attachmentCounter = 0; attachmentCounter < attachments.length; attachmentCounter++){
+        
                 //check if isUploaded key is present or not and isUploaded should be false and checks type of attachment 
                 if(attachments[attachmentCounter].hasOwnProperty("isUploaded") && !attachments[attachmentCounter].isUploaded && attachments[attachmentCounter].type !== "link"){
-
+        
                     //checks if document is present or not
                     let documentExist = await getDocumentStatus(attachments[attachmentCounter].sourcePath)
                     //if present then push object to new array and update isUploaded to true
@@ -134,10 +155,10 @@ const filePathUrl = "https://samikshaprod.blob.core.windows.net/samiksha/";
                     attachmentsPresent.push(attachments[attachmentCounter])
                 }
             }
-
+        
             return attachmentsPresent;
         }
-
+        
         //function to check if document exists
         function getDocumentStatus (sourcePath) {
             return new Promise(async (resolve, reject) => {
@@ -155,6 +176,7 @@ const filePathUrl = "https://samikshaprod.blob.core.windows.net/samiksha/";
                         if (err) {
                             result.success = false;
                         } else {
+                            console.log(data.statusCode)
                             if( data.statusCode === 200 ) {
                                 result.success = true;
                             } else {
@@ -175,18 +197,7 @@ const filePathUrl = "https://samikshaprod.blob.core.windows.net/samiksha/";
                 }
             })
         }
-
-        fs.writeFile(
-            'updatedProjectWithAttachments.json',
-
-            JSON.stringify({updatedProjectIds: UpdatedProjectId}),
-
-            function (err) {
-                if (err) {
-                    console.error('Crap happens');
-                }
-            }
-        );
+        
         
         console.log("Updated Projects", UpdatedProjectId.length)
         console.log("finished project attachment deletion based on isUploaded:= false, completed...")
