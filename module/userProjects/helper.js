bbc3822a243d16f2fa66c5a677f219b9970d66f5
VISHNU <apple@Apples-MacBook-Pro.local>
2026-04-28 16:20:16 +0530

    profile data update fix after teacher subrole changes

--- module/userProjects/helper.js
+++ module/userProjects/helper.js
@@ -3756,9 +3756,11 @@ function _updateUserProfileBasedOnUserRoleInfo(userProfile, userRoleInformation)
                     if(userProfile.profileUserTypes && Array.isArray(userProfile.profileUserTypes) && userProfile.profileUserTypes.length >0) {
                         if(!_.find(userProfile.profileUserTypes, { 'type': subRole.toLowerCase() }) && !_.find(userProfile.profileUserTypes, { 'subType': subRole.toLowerCase() })) { 
                             updateUserProfileRoleInformation = true; // Need to update userProfile.profileUserTypes
-                            if(subRole.toUpperCase() === "TEACHER") { // If subRole is not teacher
+                            
+                            // If subRole is teacher or teacher subType
+                            if(subRole.toUpperCase().startsWith("TEACHER")) { 
                                 userProfile.profileUserTypes.push({
-                                    "subType" : null,
+                                    "subType" : subRole.toUpperCase() === "TEACHER" ? null : subRole.toLowerCase(),
                                     "type" : "teacher"
                                 })
                             } else { // If subRole is not teacher
@@ -3771,9 +3773,11 @@ function _updateUserProfileBasedOnUserRoleInfo(userProfile, userRoleInformation)
                     } else { // Make a new entry if userProfile.profileUserTypes is empty or does not exist.
                         updateUserProfileRoleInformation = true; // Need to update userProfile.profileUserTypes
                         userProfile.profileUserTypes = new Array;
-                        if(subRole.toUpperCase() === "TEACHER") { // If subRole is teacher
+                        
+                        // If subRole is teacher or teacher subType
+                        if(subRole.toUpperCase().startsWith("TEACHER")) { 
                             userProfile.profileUserTypes.push({
-                                "subType" : null,
+                                "subType" : subRole.toUpperCase() === "TEACHER" ? null : subRole.toLowerCase(),
                                 "type" : "teacher"
                             })
                         } else { // If subRole is not teacher
@@ -3891,3 +3895,4 @@ function _updateUserProfileBasedOnUserRoleInfo(userProfile, userRoleInformation)
         }
     })
 }
+
